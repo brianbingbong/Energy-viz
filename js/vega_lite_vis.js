375d8eb720ac7aa03ab4338488aefea0ff82a44a
Brian Li Ong <bong0005@student.monash.edu>
2021-09-27 21:41:59 +1000

    Clean up stacked bar chart with brush interaction

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -4,6 +4,11 @@ vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 }).catch(console.error);
 
 var vg_2 = "data/positive-negative-bar-chart.vg.json";
-vegaEmbed("#positive-negative-bar-chart", vg_2).then(function(result) {
+vegaEmbed("#positive_negative_bar_chart", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "data/stacked-bar-chart.vg.json";
+vegaEmbed("#stacked_bar_chart", vg_3).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
