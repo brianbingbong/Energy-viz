e221ecc76fefab7b5d69a80229316b4d07fea8ed
Brian Li Ong <bong0005@student.monash.edu>
2021-09-27 15:00:37 +1000

    Added positive negative bar chart shell

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,7 +3,7 @@ vegaEmbed("#choropleth_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "data/tester.vg.json";
-vegaEmbed("#tester", vg_2).then(function(result) {
+var vg_2 = "data/positive-negative-bar-chart.vg.json";
+vegaEmbed("#positive-negative-bar-chart", vg_2).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
