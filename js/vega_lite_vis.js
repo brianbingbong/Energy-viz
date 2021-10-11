7b4c3e8e42c44505273f99f678f426f208f5c03e
Brian Li Ong <bong0005@student.monash.edu>
2021-10-12 00:00:18 +1100

    fix some urls to reference raw data and add some charts to index page

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -17,3 +17,13 @@ var vg_4 = "data/waffle-chart.vg.json";
 vegaEmbed("#waffle_chart", vg_4).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_5 = "data/pie.vg.json";
+vegaEmbed("#pie_chart", vg_5).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "data/scatter-plot.vg.json";
+vegaEmbed("#scatter_plot", vg_6).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
